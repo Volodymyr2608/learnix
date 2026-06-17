19bc3c0a1e97cc2c60b432935ae477238cf00204
Volodymyr Pelykh <volodymyrp8283@gmail.com>
2026-06-18 00:09:51 +0300

    feat(api): student router with getProgressStats

---
+++ server/api/routers/student.ts
@@ -0,0 +1,13 @@
+import { createTRPCRouter, studentProcedure } from "@/server/api/trpc";
+import { studentService } from "@/server/services/student/student.service";
+import { handleServiceError } from "@/server/utils/handleServiceError";
+
+export const studentRouter = createTRPCRouter({
+  getProgressStats: studentProcedure.query(async ({ ctx }) => {
+    try {
+      return await studentService.getProgressStats(ctx.session.user.id);
+    } catch (error) {
+      handleServiceError(error);
+    }
+  }),
+});
